132f5e2dd40b5c61a8c8cb8db0f8d3b0371f34b0
Dan Lemon <daniel.lemon@amazee.com>
2017-01-30 06:32:32 +0100

    Clear EXIF Image Data

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,7 @@ alias cls='clear' # Good 'ol Clear Screen command
 
 alias sweep='find "$(pwd -P)" -type d -exec dot_clean -m "{}" \;'
 
+function clear-image-exif-data() {
+  exiftool -all= $1
+  rm $1'_original'
+}
