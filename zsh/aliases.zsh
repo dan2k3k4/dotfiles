71100b0c5d1819a99d3e94834049e5dd4999e994
Dan Lemon <daniel.lemon@amazee.com>
2017-06-07 17:47:50 +0200

    Add alias for chrome and chrome-canary

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,10 @@ alias cls='clear' # Good 'ol Clear Screen command
 
 alias sweep='find "$(pwd -P)" -type d -exec dot_clean -m "{}" \;'
 
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+
+alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
+
 function clear-image-exif-data() {
   exiftool -all= $1
   rm $1'_original'
