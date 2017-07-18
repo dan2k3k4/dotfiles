7dbacd0220688981e1323b93b07e82305fb27ab5
Dan Lemon <daniel.lemon@amazee.com>
2017-07-18 15:12:11 +0200

    Update git with diff-so-fancy, update vim settings for indentation

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,10 @@ alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
 alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
 
+alias psd="open -a /Applications/Adobe\ Photoshop\ CC\ 2017/Adobe\ Photoshop\ CC\ 2017.app"
+
+alias gam="~/.dotfiles/bin/gam/gam"
+
 function clear-image-exif-data() {
   exiftool -all= $1
   rm $1'_original'
