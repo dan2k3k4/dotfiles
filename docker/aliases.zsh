5346797aca63bc26d707877d37dfaf582a3613aa
Dan Lemon <daniel.lemon@amazee.com>
2018-05-05 12:18:19 +0200

    Updates

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,7 +1,10 @@
 ## Docker alias
-alias dssh='docker-compose exec --user drupal drupal bash'
+alias dssh='docker-compose exec cli bash'
 alias dstopall='docker stop $(docker ps -a -q)'
 alias dup='docker-compose up -d'
 alias dstop='docker-compose stop'
 alias d='docker $*'
 alias d-c='docker-compose $*'
+
+## Amazee IO Lagoon Pygmy
+alias aio='pygmy $*'
