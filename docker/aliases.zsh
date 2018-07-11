5e10174b7d054c188e6af24d61d82cff635737d4
Dan Lemon <daniel.lemon@amazee.com>
2018-07-11 14:15:46 +0200

    Improve dssh alias to work with non-lagoon and lagoon projects

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,5 +1,8 @@
-## Docker alias
-alias dssh='docker-compose exec cli bash'
+## Docker aliases
+function dssh() {
+  docker-compose exec cli bash || docker-compose exec --user drupal drupal bash
+}
+
 alias dstopall='docker stop $(docker ps -a -q)'
 alias dup='docker-compose up -d'
 alias dstop='docker-compose stop'
