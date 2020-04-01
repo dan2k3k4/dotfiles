7dbdf3428231c5e7065c6027b7ddaaafd87ab22d
Dan Lemon <daniel.lemon@amazee.com>
2020-04-01 16:24:51 +0200

    Setup to allow oh-my-zsh to work via manual install

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
