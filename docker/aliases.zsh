b51768bea12617bd601eb08d42f63b81218fa1a8
Dan Lemon <daniel.lemon@amazee.com>
2021-11-25 20:02:35 +0100

    Update Brewfile

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,13 +1,13 @@
 ## Docker aliases
 function dssh() {
-  docker-compose exec cli bash || docker-compose exec --user drupal drupal bash
+  docker compose exec cli bash || docker compose exec --user drupal drupal bash
 }
 
 alias dstopall='docker stop $(docker ps -a -q)'
-alias dup='docker-compose up -d'
-alias dstop='docker-compose stop'
+alias dup='docker compose up -d'
+alias dstop='docker compose stop'
 alias d='docker $*'
-alias d-c='docker-compose $*'
+alias d-c='docker compose $*'
 
 ## Amazee IO Lagoon Pygmy
 alias aio='pygmy $*'
